069d9e952021d75f1940b10d1f9da919d2c6f816
unilock <unilock@fennet.rentals>
2024-06-18 10:36:49 -0400

    Forge: event fixes
    Properly register LootConditionTypes / LootFunctionTypes via the mod event bus
    
    Unsurprisingly, the Forge docs are wrong, so we have to listen to EntityJoinLevelEvent as well as OnDatapackSyncEvent for sending data
    
    Signed-off-by: unilock <unilock@fennet.rentals>

--- forge/src/main/java/fzzyhmstrs/emi_loot/forge/events/EMILootForgeEvents.java
+++ forge/src/main/java/fzzyhmstrs/emi_loot/forge/events/EMILootForgeEvents.java
@@ -1,14 +1,17 @@
 package fzzyhmstrs.emi_loot.forge.events;
 
 import fzzyhmstrs.emi_loot.EMILoot;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraftforge.event.OnDatapackSyncEvent;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.registries.RegisterEvent;
 
 public class EMILootForgeEvents {
     @SubscribeEvent
-    public void onRegister(RegisterEvent event) {
-        EMILoot.register();
+    public void onEntityLevelWorld(EntityJoinLevelEvent event) {
+        if (event.getEntity() instanceof ServerPlayerEntity player) {
+            EMILoot.parser.registerServer(player);
+        }
     }
 
     @SubscribeEvent
